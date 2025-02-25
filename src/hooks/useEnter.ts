bf49ae9957c7894d8008035e58ba90d1631aeae6
Yurii Tereshchenko <yurii.tereshchenko.w@gmail.com>
2025-02-25 02:59:59 +0100

    fix useEnter hook functionality

--- src/hooks/useEnter.ts
+++ src/hooks/useEnter.ts
@@ -1,4 +1,11 @@
 import { useEventListener } from "./useEventListener";
 
 export const useEnter = (handler: () => void, deps?: unknown[]) =>
-  useEventListener(document, "keydown", handler, deps);
+  useEventListener(
+    document,
+    "keydown",
+    (e) => {
+      if (e.key === "Enter") handler();
+    },
+    deps,
+  );
